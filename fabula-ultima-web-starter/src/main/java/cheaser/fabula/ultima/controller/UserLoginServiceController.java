7eec3f4c890e15dd134411d657845f9f0939cec6
Cheaser <3634065145@qq.com>
2025-07-04 01:20:10 +0800

    feat(user): add sign-up endpoint and change controller to RestController

--- fabula-ultima-web-starter/src/main/java/cheaser/fabula/ultima/controller/UserLoginServiceController.java
+++ fabula-ultima-web-starter/src/main/java/cheaser/fabula/ultima/controller/UserLoginServiceController.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import static cheaser.fabula.ultima.enums.FabulaUltimaWebErrorCodeEnum.LOGIN_PASSWORD_IS_NULL;
 import static cheaser.fabula.ultima.enums.FabulaUltimaWebErrorCodeEnum.LOGIN_USERNAME_IS_NULL;
 
 @Slf4j
-@Controller("/users")
+@RestController
+@RequestMapping("/user")
 public class UserLoginServiceController {
 
     private UserLoginService loginService;
@@ -33,6 +35,19 @@ public class UserLoginServiceController {
             return Result.success(result);
         }catch (Exception e){
             log.error("UserLoginServiceController#userLogin exception:{}", e.getMessage());
+            return Result.failure(e.getMessage());
+        }
+    }
+    
+    @RequestMapping("/signUp")
+    public Result<String> signUp(@RequestParam String userName, @RequestParam String password){
+        try{
+            Assert.notNull(userName, LOGIN_USERNAME_IS_NULL.getErrorMessage());
+            Assert.notNull(password, LOGIN_PASSWORD_IS_NULL.getErrorMessage());
+
+            return Result.success(null);
+        }catch (Exception e){
+            log.error("UserLoginServiceController#signUp exception:{}", e.getMessage());
             return Result.failure(e.getMessage());
         }
     }
