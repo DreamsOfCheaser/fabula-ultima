edec2ceebad2658fe3079a44c96692f162c457d0
修克 <zhouxinwei.zxw@taobao.com>
2025-07-07 16:16:25 +0800

    feat(pom.xml): 添加依赖于 `fabula-ultima-web-common`
    refactor(UserLoginServiceController): 移除未使用的导入并优化成员变量声明
    
    feat(UserLoginService): 增加用户登录名格式校验
    
    feat(FabulaUltimaWebErrorCodeEnum): 添加新的错误码枚举值

--- fabula-ultima-web-common/src/main/java/cheaser/fabula/ultima/enums/FabulaUltimaWebErrorCodeEnum.java
+++ fabula-ultima-web-common/src/main/java/cheaser/fabula/ultima/enums/FabulaUltimaWebErrorCodeEnum.java
@@ -6,19 +6,21 @@ public enum FabulaUltimaWebErrorCodeEnum {
 
     LOGIN_USERNAME_IS_NULL("LOGIN_USERNAME_IS_NULL", "用户名为空"),
 
+    LOGIN_USERNAME_PATTERN_NOT_MATCH("LOGIN_USERNAME_PATTERN_NOT_MATCH", "用户名格式错误"),
+
     LOGIN_PASSWORD_IS_NULL("LOGIN_PASSWORD_IS_NULL", "密码为空");
 
     /**
      * 错误码
      */
     @Getter
-    private String errorCode;
+    private final String errorCode;
 
     /**
      * 错误信息
      */
     @Getter
-    private String errorMessage;
+    private final String errorMessage;
 
     FabulaUltimaWebErrorCodeEnum(String errorCode, String errorMessage) {
         this.errorCode = errorCode;
--- fabula-ultima-web-starter/src/main/java/cheaser/fabula/ultima/controller/UserLoginServiceController.java
+++ fabula-ultima-web-starter/src/main/java/cheaser/fabula/ultima/controller/UserLoginServiceController.java
@@ -4,7 +4,6 @@ import cheaser.fabula.ultima.user.UserLoginService;
 import cheaser.utils.common.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +17,7 @@ import static cheaser.fabula.ultima.enums.FabulaUltimaWebErrorCodeEnum.LOGIN_USE
 @RequestMapping("/user")
 public class UserLoginServiceController {
 
-    private UserLoginService loginService;
+    private final UserLoginService loginService;
 
     @Autowired
     public UserLoginServiceController(UserLoginService userLoginService){
@@ -28,9 +27,6 @@ public class UserLoginServiceController {
     @RequestMapping("/login")
     public Result<String> userLogin(@RequestParam String userName, @RequestParam String password){
         try{
-            Assert.notNull(userName, LOGIN_USERNAME_IS_NULL.getErrorMessage());
-            Assert.notNull(password, LOGIN_PASSWORD_IS_NULL.getErrorMessage());
-
             String result = loginService.userLoginByUserNameAndPassword(userName, password);
             return Result.success(result);
         }catch (Exception e){
